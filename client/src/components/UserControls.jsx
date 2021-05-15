e09e4307474db8cca376225f8cbd5518e3483bfc
Anna-Mariia <annamariypetrik@gmail.com>
2021-05-15 15:07:36 +0300

    StartPage

--- client/src/components/UserControls.jsx
+++ client/src/components/UserControls.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import   '../styles/stylesLibrary/css/main-styles.css';
 import '../styles/stylesLibrary/css/reset.css';
-import '../styles/stylesLibrary/css/login.css';
 import {useDispatch,useSelector} from 'react-redux';
 import Avatars from '../store/avatar-ref';
 
@@ -9,46 +8,40 @@ const UserControls = (props)=>{
     const dispatch = useDispatch();
     let avatarIndex = useSelector(state => state.avatarIndex);
 
-    const handleClick1 = () =>{
+    const SwithAvatarRigth = () =>{
       dispatch({type:"+AvatarIndex", payload: 1});
       console.log(avatarIndex)
     };
-    const handleClick2 = () =>{
+    const SwithAvatarLeft = () =>{
       dispatch({type:"-AvatarIndex", payload:  1});
       console.log(avatarIndex)
     };
 
-    const name = useSelector(state=> state.name)
-    const handleChange = (e) => {
-      dispatch({type:"setName", payload: e.target.value});
-    };
-    
-    
   return (
     <div className="container-all container-login "> 
-        <div className='wrapper'>
+        
           <div className='div-control'>
             <button data-arrow='left' 
               className="button-bgi-arrow1 all-button button-arrow"
-              onClick={handleClick1}
+              onClick={SwithAvatarRigth}
             ></button>
             <div className="container-avatar container-all"
             style={{backgroundImage : `url(${process.env.PUBLIC_URL + Avatars[avatarIndex]})`}}>
             </div>
             <button 
               className="all-button button-arrow button-bgi-arrow2"
-              onClick={handleClick2}
+              onClick={SwithAvatarLeft}
             ></button>
           </div>
           <div className="div">
             <input  
               type='text' 
-              pattern='Ссылка на комнату' 
+              placeholder='Ссылка на комнату' 
               className="input-all input input-name" 
-              onChange={handleChange}
+              ref={props.inputRef}
             />
           </div>
-        </div>
+        
       </div>
   )
 }
--- client/src/pages/StartPage.jsx
+++ client/src/pages/StartPage.jsx
@@ -1,16 +1,26 @@
 import React from 'react'
 import { Logo, UserSettings, UserControls } from '../components';
-import '../styles/stylesLibrary/css/startPage.css'
+import '../styles/stylesLibrary/css/main-styles.css';
+import '../styles/stylesLibrary/css/reset.css';
 
 function StartPage() {
+  const inputNameRef = React.useRef();
+
+  const handleGameClick = () => {
+    const username = inputRef.current.value;
+    if(username.length) {
+      dispatch({
+        type: ""
+      })
+    }
+  }
+
   return (
     <div style={{ height: window.innerHeight }} className="wrapper">
                   <Logo />
-                  <div style={{position:"relative"}}>
-                  <form className="wrapper2" onSubmit={()=>{}}>
-                      <UserControls/>
-                      <UserSettings />
-                  </form>
+                  <div className="wrapper2" onSubmit={()=>{}}>
+                      <UserControls inputRef={inputRef}/>
+                      <UserSettings handleGameClick={handleGameClick}/>
                   </div>
               </div>
   )
